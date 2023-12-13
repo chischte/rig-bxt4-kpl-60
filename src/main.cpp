071561cb28cc051dce945aacf0c66abf793cd0ee
Michael Wettstein <chischte@gmx.ch>
2023-12-13 10:33:49 +0100

    Increase delay time as requested

--- src/main.cpp
+++ src/main.cpp
@@ -996,7 +996,7 @@ class Festklemmen : public Cycle_step {
     delay_cycle_step.set_unstarted();
   };
   void do_loop_stuff() {
-    if (delay_cycle_step.delay_time_is_up(400)) {
+    if (delay_cycle_step.delay_time_is_up(1200)) {
       set_loop_completed();
     }
   };
